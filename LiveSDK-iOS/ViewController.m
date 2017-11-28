2d83c3ddbdd6ace79a656fac78a23b1c50e1b633
ftao <fft1026@163.com>
2017-11-28 16:28:50 +0800

    [Modify]设置为Pod项目

--- LiveSDK-iOS/ViewController.m
+++ LiveSDK-iOS/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import <PLVLiveSDK/PLVLiveSDK.h>
 
-@interface ViewController ()
+@interface ViewController () <PLVSocketIODelegate>
+
+@property (nonatomic, strong) PLVSocketIO *socketIO;
 
 @end
 
@@ -17,6 +20,31 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [PLVLiveConfig liveConfigWithAppId:@"666" appSecret:@"dasdasdasdasdasd"];
+    NSLog(@"appId:%@, appSecret:%@",[PLVLiveConfig sharedInstance].appId,[PLVLiveConfig sharedInstance].appSecret);
+    
+    //[PLVLiveAPI loadChannelInfoWithUserId:@"asd" channelId:103117 completion:^(PLVLiveChannel *channel) {
+    //    NSLog(@"listUsers:%@",channel);
+    //} failure:^(PLVLiveErrorCode errorCode, NSString *description) {
+    //    NSLog(@"errorCode:%ld, descripption:%@",errorCode,description);
+    //}];
+    
+    self.socketIO = [[PLVSocketIO alloc] initSocketIOWithConnectToken:@"" enableLog:NO];
+    self.socketIO.delegate = self;
+    [self.socketIO connect];
+}
+
+#pragma mark - <PLVSocketIODelegate>
+
+- (void)socketIO:(PLVSocketIO *)socketIO didConnectWithInfo:(NSString *)info {
+    NSLog(@"socketIO:didConnectWithInfo: %@",info);
+}
+
+- (void)socketIO:(PLVSocketIO *)socketIO didReceiveChatMessage:(PLVSocketChatRoomObject *)chatRoomObject {
+    NSLog(@"socketIO:didReceiveChatMessage: type:%lu, event:%@",chatRoomObject.eventType,chatRoomObject.event);
+    NSDictionary *dict = chatRoomObject.jsonDict;
+    NSLog(@"%@",dict);
 }
 
 
